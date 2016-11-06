bd569a3cefe3ddbcfeef3c0224da973c8df363a4
Ray1235 <ray1235@me.com>
2016-11-06 13:11:14 +0100

    Added zlib

--- Syntic/linker/linker.cpp
+++ Syntic/linker/linker.cpp
@@ -1,7 +1,10 @@
-
-
+#include <zlib.h>
+#include <iostream>
+#define printline(x) std::cout << x << std::endl;
 int main(int argc, char * argv[])
 {
+	printline("== Syntic Linker ==");
+	printline("using zlib version " << zlibVersion());
     return 0;
 }
 
