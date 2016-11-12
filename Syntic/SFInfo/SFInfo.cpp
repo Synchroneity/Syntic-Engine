bcf4825d552d1f1d884b816e2d9cf6d03883e1ea
Ray1235 <ray1235@me.com>
2016-11-12 20:15:09 +0100

    SFInfo extracts sf files

--- Syntic/SFInfo/SFInfo.cpp
+++ Syntic/SFInfo/SFInfo.cpp
@@ -1,11 +1,51 @@
 // SFInfo.cpp : Defines the entry point for the console application.
 //
+#define _CRT_SECURE_NO_WARNINGS
 
 #include "stdafx.h"
 
 #include <iostream>
+#include <Windows.h>
 
+#include <zlib.h>
 #include "../Shared/SF.h"
+
+char* PathGetParentDir(char path[])
+{
+	char result[512];
+	int len = strlen(path);
+	int lastIndex = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (path[i] == '/' || path[i] == '\\')
+		{
+			lastIndex = i;
+		}
+	}
+	strncpy_s(result, path, lastIndex + 1);
+	//printf("PathGetParentDir() result: %s", result);
+	return result;
+}
+// stolen from linkermod
+int FS_CreatePath(const char* targetPath)
+{
+	int len = strlen(targetPath);
+	for (int i = 0; i < len; i++)
+	{
+		if (targetPath[i] == '/' || targetPath[i] == '\\')
+		{
+			char buf[1024] = "";
+			strncpy(buf + strlen(buf), targetPath, i);
+
+			if (!CreateDirectoryA(buf, 0) && GetLastError() != ERROR_ALREADY_EXISTS)
+			{
+				return GetLastError();
+			}
+		}
+	}
+
+	return 0;
+}
 
 void PrintUsage()
 {
@@ -14,7 +54,10 @@ void PrintUsage()
 
 void PrintAppInfo()
 {
-
+	std::cout << " *================*" << std::endl;
+	std::cout << " *    SFInfo      *" << std::endl;
+	std::cout << " *   for Syntic   *" << std::endl;
+	std::cout << " *================*" << std::endl;
 }
 
 sftHeader_s ReadHeader(FILE * f)
@@ -23,10 +66,8 @@ sftHeader_s ReadHeader(FILE * f)
 	unsigned char version;
 	unsigned short fileCount;
 	fread(magic, sizeof(SFMagic), sizeof(char), f);
-	//fseek(f, 6, SEEK_SET);
 	fread(&version, 1, sizeof(unsigned char), f);
 	fread(&fileCount, 1, sizeof(unsigned short), f);
-	//fileCount = _byteswap_ushort(fileCount);
 	std::cout << "Magic: " << magic << std::endl << "Version: " << (int)version << std::endl << "File Count: " << fileCount << std::endl;
 	sftHeader_s header;
 	memset(&header, 0, sizeof(header));
@@ -51,7 +92,6 @@ sftFileEntry_s ReadFileEntry(FILE * f)
 	fread((void *)name, 256, 1, f);
 	fread((void *)&type, 1, sizeof(unsigned char), f);
 	fread((void *)&offset, 1, sizeof(unsigned long), f);
-	//std::cout << "Name: " << name << std::endl << "Type: " << (int)type << std::endl << "Offset: " << offset << std::endl;
 	strcpy_s(fe.name, name);
 	fe.type = type;
 	fe.offset = offset;
@@ -61,10 +101,10 @@ sftFileEntry_s ReadFileEntry(FILE * f)
 sfFile_s ReadCompressedFile(FILE * f)
 {
 	/*
-				fwrite((void *)&f.uncompressedSize, 1, sizeof(f.uncompressedSize), out);
-			fwrite((void *)&f.compressedSize, 1, sizeof(f.compressedSize), out);
-			fwrite((void *)f.compressedFile, f.compressedSize, 1, out);
-			*/
+	fwrite((void *)&f.uncompressedSize, 1, sizeof(f.uncompressedSize), out);
+	fwrite((void *)&f.compressedSize, 1, sizeof(f.compressedSize), out);
+	fwrite((void *)f.compressedFile, f.compressedSize, 1, out);
+	*/
 	sfFile_s sff;
 	memset(&sff, 0, sizeof(sff));
 	fread((void *)&sff.uncompressedSize, 1, sizeof(sff.uncompressedSize), f);
@@ -77,6 +117,7 @@ sfFile_s ReadCompressedFile(FILE * f)
 
 int main(int argc, char * argv[])
 {
+	PrintAppInfo();
 	if (argc < 2)
 	{
 		PrintUsage();
@@ -111,13 +152,50 @@ int main(int argc, char * argv[])
 		ft = NULL;
 		// WE FINISHED READING THE FILE ENTRY TABLE AND WE'VE LOADED IT TO MEMORY, WE CAN CLOSE THE STREAM NOW
 		fopen_s(&f, sf, "rb");
-
+		if (f == NULL)
+		{
+			std::cout << "ERROR: " << sft << " exists, but not " << sf << "!" << std::endl;
+			continue;
+		}
 		sfFile_s *sfFiles = new sfFile_s[sftFile.header.fileCount];
 		for (int ip = 0; ip < sftFile.header.fileCount; ip++)
 		{
+			char resultOut[512];
+			char resultOutNoFSpec[512];
 			std::cout << std::endl << "File #" << ip << ":" << std::endl;
 			std::cout << "Name: " << sftFile.entries[ip].name << std::endl << "Type: " << (int)sftFile.entries[ip].type << std::endl << "Offset: " << sftFile.entries[ip].offset << std::endl;
 			sfFiles[ip] = ReadCompressedFile(f);
+			sprintf_s(resultOut, "%s_DUMP/%s", argv[i + 1], sftFile.entries[ip].name);
+			std::cout << "File uncompression result: ";
+			char *uncompFile = new char[sfFiles[ip].uncompressedSize];
+			uLongf uncompSize = sfFiles[ip].uncompressedSize;
+			int z_result = uncompress((Bytef *)uncompFile, &uncompSize, sfFiles[ip].compressedFile, sfFiles[ip].compressedSize);
+			//printf("%s\n", uncompFile);
+			strcpy_s(resultOutNoFSpec, PathGetParentDir(resultOut));
+			//PathRemoveFileSpec(resultOutNoFSpec);
+			switch (z_result)
+			{
+			case Z_OK:
+				printf("ok\n");
+				FS_CreatePath(resultOutNoFSpec);
+				FILE * uncompfOut;
+				fopen_s(&uncompfOut, resultOut, "wb");
+				if (uncompfOut != NULL) {
+					fwrite((void *)uncompFile, uncompSize, sizeof(char), uncompfOut);
+					fclose(uncompfOut);
+				}
+				uncompfOut = NULL;
+				break;
+			case Z_MEM_ERROR:
+				printf("out of memory\n");
+				break;
+			case Z_BUF_ERROR:
+				printf("output buffer wasn't large enough!\n");
+				break;
+			default:
+				printf("unknown exception code: %d\n", z_result);
+				break;
+			}
 		}
 
 		fclose(f);
